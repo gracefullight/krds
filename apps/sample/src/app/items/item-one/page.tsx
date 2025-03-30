425d5c9d09eee4d9f109b81d501d654eb7c1a95d
gracefullight <gracefullight.dev@gmail.com>
2025-03-30 20:10:39 +0900

    feat: ✨ item title

--- apps/sample/src/app/items/item-one/page.tsx
+++ apps/sample/src/app/items/item-one/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
+
 import { Typography } from "@mui/material";
+
+export const metadata: Metadata = {
+  title: "Item One",
+};
 
 export default function ItemOnePage() {
   return <Typography>Item One</Typography>;
--- apps/sample/src/app/items/item-two/page.tsx
+++ apps/sample/src/app/items/item-two/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
+
 import { Typography } from "@mui/material";
+
+export const metadata: Metadata = {
+  title: "Item Two",
+};
 
 export default function ItemTwoPage() {
   return <Typography>Item Two</Typography>;
