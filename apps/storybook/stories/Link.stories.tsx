01d6f16d1f815ea7882f3d87dc0b88433ef61b19
gracefullight <gracefullight.dev@gmail.com>
2025-04-06 16:21:00 +0900

    feat: ✨ link story

---
+++ apps/storybook/stories/Link.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta } from "@storybook/react";
+
+import { Link } from "@mui/material";
+
+const meta: Meta<typeof Link> = {
+  title: "KRDS/Link",
+  component: Link,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/FQZqKPOEJVqFCFglsC5VUt/KRDS_v1.0.0--Community-?node-id=4847-27045&t=5iLlxTAzJnfFkDu5-4",
+    },
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    type: {
+      options: ["default", "subtle", "subtle-none"],
+      control: {
+        type: "radio",
+      },
+    },
+    // @ts-expect-error
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "radio" },
+    },
+    icon: {
+      options: ["none", "external"],
+      control: { type: "radio" },
+    },
+  },
+};
+
+export default meta;
+
+export const ExampleLink = {
+  args: {
+    children: "링크",
+    type: "default",
+    size: "medium",
+    icon: "none",
+  },
+};
