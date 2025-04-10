420585f604219cac8b1d3832b28de0e573c50b08
gracefullight <gracefullight.dev@gmail.com>
2025-04-11 00:56:05 +0900

    feat: ✨ select

---
+++ apps/storybook/stories/select.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta } from "@storybook/react";
+
+import { MenuItem, Select } from "@mui/material";
+
+const meta: Meta<typeof Select> = {
+  title: "KRDS/Select",
+  component: Select,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/FQZqKPOEJVqFCFglsC5VUt/KRDS_v1.0.0--Community-?node-id=5173-4612&t=xytebmefZpUAfFe3-4",
+    },
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    disabled: {
+      control: { type: "boolean" },
+    },
+    displayEmpty: {
+      control: { type: "boolean" },
+    },
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "radio" },
+    },
+    value: {
+      control: { type: "text" },
+    },
+  },
+};
+
+export default meta;
+
+export const ExampleSelect = {
+  args: {
+    disabled: false,
+    displayEmpty: true,
+    size: "medium",
+    value: "",
+    children: [
+      <MenuItem key="0" value="">
+        선택해주세요.
+      </MenuItem>,
+      <MenuItem key="1" value="1">
+        옵션 1
+      </MenuItem>,
+      <MenuItem key="2" value="2">
+        옵션 2
+      </MenuItem>,
+      <MenuItem key="3" value="3">
+        옵션 3
+      </MenuItem>,
+    ],
+  },
+};
--- packages/krds/src/components/mui-select.ts
+++ packages/krds/src/components/mui-select.ts
@@ -1,10 +1,85 @@
-import type { Components } from "@mui/material";
+import type { Components, Theme } from "@mui/material";
 
-declare module "@mui/material/Select" {}
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { selectClasses, svgIconClasses } from "@mui/material";
+import { getPalette } from "#/design-tokens/palettes";
+import { getTypography } from "#/design-tokens/typography";
+
+declare module "@mui/material/Select" {
+  interface InputBasePropsSizeOverrides {
+    large: true;
+  }
+}
 
 export const MuiSelect: Components["MuiSelect"] = {
-  defaultProps: {},
+  defaultProps: {
+    displayEmpty: true,
+    size: "medium",
+    IconComponent: KeyboardArrowDownIcon,
+  },
   styleOverrides: {
-    root: {},
+    root: ({ theme }) => ({
+      [`& .${selectClasses.select}`]: {
+        ...getTypography("pc.label.large"),
+        // color: getPalette("text.disabled"),
+      },
+
+      [(theme as Theme).breakpoints.down("medium")]: {
+        [`& .${selectClasses.select}`]: {
+          ...getTypography("mobile.label.large"),
+        },
+      },
+
+      [`& .${svgIconClasses.root}`]: {
+        color: getPalette("icon.gray"),
+        right: "16px",
+      },
+
+      variants: [
+        {
+          props: { size: "large" },
+          style: {
+            [`& .${selectClasses.select}`]: {
+              padding: "13.5px 16px",
+              paddingRight: "44px !important",
+            },
+
+            [`& .${svgIconClasses.root}`]: {
+              width: "24px",
+              height: "24px",
+              top: "calc(50% - 12px)",
+            },
+          },
+        },
+        {
+          props: { size: "medium" },
+          style: {
+            [`& .${selectClasses.select}`]: {
+              padding: "11px 16px",
+              paddingRight: "40px !important",
+            },
+            [`& .${svgIconClasses.root}`]: {
+              width: "20px",
+              height: "20px",
+              top: "calc(50% - 10px)",
+            },
+          },
+        },
+        {
+          props: { size: "small" },
+          style: {
+            [`& .${selectClasses.select}`]: {
+              padding: "8.5px 16px",
+              paddingRight: "36px !important",
+            },
+            [`& .${svgIconClasses.root}`]: {
+              width: "16px",
+              height: "16px",
+              top: "calc(50% - 8px)",
+            },
+          },
+        },
+      ],
+    }),
   },
 };
